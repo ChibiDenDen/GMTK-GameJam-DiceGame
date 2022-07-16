b492f473a67e0aa0e045102469a5eecd3f169d66
Lotem Amiel <lotem3655@gmail.com>
2022-07-16 13:58:45 +0300

    Add in-game menu

--- Scenes/Game/UIViewport.gd
+++ Scenes/Game/UIViewport.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
 @onready var inventory = $Equipment
+@onready var menu = $Menu
+
 
 func toggle_inventory():
 	inventory.visible = !inventory.visible
@@ -9,7 +11,17 @@ func toggle_inventory():
 	else:
 		get_parent().process_mode = Node.PROCESS_MODE_ALWAYS
 
+func toggle_menu():
+	menu.visible = !menu.visible
+	if menu.visible:
+		get_parent().process_mode = Node.PROCESS_MODE_DISABLED
+	else:
+		get_parent().process_mode = Node.PROCESS_MODE_ALWAYS
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("Inventory"):
 		toggle_inventory()
+	if Input.is_action_just_pressed("Exit"):
+		toggle_menu()
+
