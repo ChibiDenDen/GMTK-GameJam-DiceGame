03420fc5f8355c9ecb035123268eb36ed7ddbc82
Dani Frank <pdaniq@gmail.com>
2022-07-16 17:28:58 +0300

    fix pause and inventory menues

--- Scenes/Game/UIViewport.gd
+++ Scenes/Game/UIViewport.gd
@@ -3,24 +3,34 @@ extends CanvasLayer
 @onready var inventory = $Equipment
 @onready var menu = $Menu
 
+func pause_game():
+	get_parent().process_mode = Node.PROCESS_MODE_DISABLED
+	$HPBar.visible = false
+
+func resume_game():
+	get_parent().process_mode = Node.PROCESS_MODE_ALWAYS
+	$HPBar.visible = true
+
 func toggle_inventory():
 	inventory.visible = !inventory.visible
 	if inventory.visible:
-		get_parent().process_mode = Node.PROCESS_MODE_DISABLED
+		pause_game()
 	else:
 		get_parent().find_child("Player").setup_items()
-		get_parent().process_mode = Node.PROCESS_MODE_ALWAYS
+		resume_game()
 
 func toggle_menu():
 	menu.visible = !menu.visible
 	if menu.visible:
-		get_parent().process_mode = Node.PROCESS_MODE_DISABLED
+		pause_game()
 	else:
-		get_parent().process_mode = Node.PROCESS_MODE_ALWAYS
+		resume_game()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("Inventory"):
-		toggle_inventory()
-	if Input.is_action_just_pressed("Exit"):
-		toggle_menu()
+	if not menu.visible:
+		if Input.is_action_just_pressed("Inventory"):
+			toggle_inventory()
+	if not inventory.visible:
+		if Input.is_action_just_pressed("Exit"):
+			toggle_menu()
